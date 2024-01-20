8420b7a80a71cb65b0b8ee93a26bc364e18b6c9a
meng <meng.liu1@bthome.com>
2024-01-20 18:21:19 +0800

    添加页面

---
+++ src/components/custom/Toolbar.jsx
@@ -0,0 +1,17 @@
+/**
+ * Author: Meng
+ * Date: 2024-01-20
+ * Modify: 2024-01-20
+ * Desc: 
+ */
+import React from 'react';
+
+function Toolbar(props) {
+  return (
+    <div className="">
+      <h1 onClick={() => alert('12345678')}>Toolbar</h1>
+    </div>
+  )
+}
+
+export default Toolbar;
